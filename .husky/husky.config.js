a5b80daca9b20eaef5593cf8a4ae01cfebd0349e
Jose Cruz <jose.crperez@gmail.com>
2022-03-23 22:44:10 -0600

    feat(finish skeleton): finish skeleton

--- .husky/husky.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    hooks: {
-        'pre-push': 'npm run lint',
-        'pre-commit': 'npm run test',
-    },
-}
--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.scss']
 })
-export class TestComponent implements OnInit {
+export class TestComponent  {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+ 
 
 }
